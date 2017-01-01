d1a24e9f6cf5874a56160e28b1a1a80ceb822ee0
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-01-01 17:15:06 -0500

    Working generate bitcoin key/address functionality

--- src/createKey.js
+++ src/createKey.js
@@ -1,11 +1,18 @@
-var bitcoin = require('bitcoinjs-lib');
-var bigi = require('bigi');
+const bitcoin = require('bitcoinjs-lib');
+const bigi = require('bigi');
 
 module.exports = function generateKey (hash_string) {
-  var hash = bitcoin.crypto.sha256(hash_string);
-  var d = bigi.fromBuffer(hash);
+  let keyPair;
+  if(hash_string === true) {
+    keyPair = bitcoin.ECPair.makeRandom(); 
+  } else {
+    let hash = bitcoin.crypto.sha256(hash_string);
+    let d = bigi.fromBuffer(hash);
+    keyPair = new bitcoin.ECPair(d);
+  }
 
-  var keyPair = new bitcoin.ECPair(d);
-  var address = keyPair.getAddress();
-  return `Generated Address ${address}`;
+  let address = keyPair.getAddress();
+  let privateKey = keyPair.toWIF();
+  return `Created Address: ${address}
+Created Key: ${privateKey}`;
 }

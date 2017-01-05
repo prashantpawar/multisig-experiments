834518f0478836b1e9dbacd5052a3e4d8e2ecf85
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-01-05 01:53:11 -0500

    Refactored create.js to be more functional

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ const utils = require('./src/utils.js');
 const mainCommand = 'contract';
 
 const defaultToEmptyArray = R.defaultTo([]);
+const printOutput = (handlerFn) => {
+  return (args, callback) => {
+    vorpal.log(handlerFn(args, callback));
+  }
+};
 
 contractCommands.map(function registerCommands(command) {
   const vc = vorpal.command(utils.prefixer(mainCommand, command.name));
   R.map(function (option) {
     vc.option(option);
   }, defaultToEmptyArray(command.options));
-  vc.action(command.handlerFn);
+  vc.action(printOutput(command.handlerFn));
   return vc;
 });
 
--- src/commands/create.js
+++ src/commands/create.js
@@ -1,5 +1,4 @@
 const R = require('ramda');
-const vorpal = require('vorpal')();
 const bitcoin = require('bitcoinjs-lib');
 const config = require('./../config.js');
 
@@ -11,10 +10,13 @@ const options = {
 module.exports = {
   name: 'create',
   handlerFn: function (args, callback) {
-    R.mapObjIndexed(function (optionArg, optionName) {
-			vorpal.log(options[optionName].call(vorpal, optionArg, config));
-    }, args.options);
+    const callOption = (optionArg, optionName) => {
+      return options[optionName].call(this, optionArg, config);
+    };
+    const processOptions = R.compose(R.join(''), R.values, R.mapObjIndexed(callOption));
+    const output = processOptions(args.options);
     callback();
+    return output;
   },
   options: [
     '-c, --createKey [hash_string]',

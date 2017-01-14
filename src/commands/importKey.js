a7f033fcd338ac4504260d7b04d3215a97ab7ef9
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-01-14 10:58:08 -0500

    Removed create and import commands

--- src/commands/importKey.js
+++
@@ -1,8 +0,0 @@
-var bitcoin = require('bitcoinjs-lib');
-var bigi = require('bigi');
-
-module.exports = function importKey(wif, config) {
-  var keyPair = bitcoin.ECPair.fromWIF(wif, config.network);
-  var address = keyPair.getAddress();
-  return `Imported Address ${address}`;
-}
